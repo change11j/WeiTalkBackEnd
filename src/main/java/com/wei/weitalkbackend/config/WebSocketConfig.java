9a1fb1de054e0871548829a2d19e4d9dedd29686
change11j <change11j@gamil.com>
2024-07-18 20:41:44 +0800

    fix:測試cicd 第二次

--- src/main/java/com/wei/weitalkbackend/config/WebSocketConfig.java
+++ src/main/java/com/wei/weitalkbackend/config/WebSocketConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.socket.config.annotation.*;
 public class WebSocketConfig implements WebSocketConfigurer {
     @Autowired
     private ChatWebSocketHandler chatWebSocketHandler;
-//    testcicd
+//    testcicd again
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
