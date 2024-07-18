93e8f785576577ff766ebe008ef97068a02a56ce
change11j <change11j@gamil.com>
2024-07-18 19:53:44 +0800

    feat:第一次上傳

--- src/main/java/com/wei/weitalkbackend/entity/Message.java
+++ src/main/java/com/wei/weitalkbackend/entity/Message.java
@@ -1,17 +1,14 @@
 package com.wei.weitalkbackend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Entity
 @Data
 @AllArgsConstructor
 public class Message {
-    @Id
     private int id;
     private String content;
     private String senderId;
